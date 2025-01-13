98c27c0116c6b46490ad06996014024e5f408541
MrPenguz <mohammadmasere@gmail.com>
2025-01-13 12:11:39 +0300

    Profile Picture commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,15 @@
-import authRoutes from './routes/auth.route.js'
-import dotenv from 'dotenv'
-import {connectDB} from "./lib/db.js"
-import express from 'express';
-dotenv.config()
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-app.use(express.json())
-app.use("/api/auth" , authRoutes)
-app.listen(PORT,() => {
-    console.log("server is running on PORT:" + PORT);
-    connectDB()
-})
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
