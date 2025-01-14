8c38008bb71704f2aff838823b11ceec7f803df9
MrPenguz <mohammadmasere@gmail.com>
2025-01-14 12:58:25 +0300

    Global state and axios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Routes , Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
@@ -6,8 +6,18 @@ import SIgnupPage from './pages/SIgnupPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { axiosInstance } from './lib/axios'
+import { useAuthStore } from './store/useAuthStore'
 
 const App = () => {
+  const {authUser , checkAuth}=useAuthStore()
+
+  useEffect(() => {
+    checkAuth();
+
+  }, [checkAuth]);
+  console.log({authUser})
+  
   return (
     <div className='text-red-500'>
       <Navbar/>
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,2 +1,5 @@
 import axios from "axios";
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+  baseURL: "http://localhost:5001/api",
+  withCredentials: true,
+});
