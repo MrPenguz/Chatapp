c29705fafe233b793e68d78ad120d51cd8073998
MrPenguz <mohammadmasere@gmail.com>
2025-03-10 15:57:51 +0300

    large photo upload fix commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,8 @@ import { app, server } from "./lib/Socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
